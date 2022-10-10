a0d37ea610052ab2217465b88636b412c1fcdf85
Elielson Nascimento <nascimento.elielson@gmail.com>
2022-10-10 00:10:29 -0300

    feat - Full Header test

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,21 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from '../App';
 
 describe('Testando o componente Header:', () => {
     it('Existem 3 imagens no componente Header', () => {
         render(<App />);
 
-        const imagemPt = screen.getByRole('img', {
-          name: /pt/i
-        });
-
-        const imagemEn = screen.getByRole('img', {
-          name: /en/i
-        });
-
-        const imagemEs = screen.getByRole('img', {
-          name: /es/i
-        });
+        const imagemPt = screen.getByRole('img', {name: /pt/i});
+        const imagemEn = screen.getByRole('img', {name: /en/i});
+        const imagemEs = screen.getByRole('img', {name: /es/i});
         
         expect(imagemPt).toBeInTheDocument();
         expect(imagemEn).toBeInTheDocument();
@@ -25,9 +17,7 @@ describe('Testando o componente Header:', () => {
     it('Existe um h1 com o nome Elielson Nascimento', () => {
         render(<App />);
 
-        const myName = screen.getByRole('heading', {
-          name: /elielson nascimento/i
-        })
+        const myName = screen.getByRole('heading', {name: /elielson nascimento/i})
 
         expect(myName).toBeInTheDocument();
     })
@@ -49,6 +39,60 @@ describe('Testando o componente Header:', () => {
 
       const contact = screen.getByRole('link', {
         name: /contato/i
+      })
+
+      expect(whoIam).toBeInTheDocument();
+      expect(skills).toBeInTheDocument();
+      expect(projects).toBeInTheDocument();
+      expect(contact).toBeInTheDocument();
+    })
+
+    it('Ao ser clicado na imagem da U.K o idioma é mudado no Header', () => {
+      render(<App />);
+
+      fireEvent.click(screen.getByRole('img', {name: /en/i}));
+
+      const whoIam = screen.getByRole('link', {
+        name: /who i am\?/i
+      })
+
+      const skills = screen.getByRole('link', {
+        name: /skills/i
+      })
+
+      const projects = screen.getByRole('link', {
+        name: /projects/i
+      })
+
+      const contact = screen.getByRole('link', {
+        name: /contact/i
+      })
+
+      expect(whoIam).toBeInTheDocument();
+      expect(skills).toBeInTheDocument();
+      expect(projects).toBeInTheDocument();
+      expect(contact).toBeInTheDocument();
+    })
+
+    it('Ao ser clicado na imagem da Espanha o idioma é mudado no Header', () => {
+      render(<App />);
+
+      fireEvent.click(screen.getByRole('img', {name: /es/i}));
+
+      const whoIam = screen.getByRole('link', {
+        name: /quien soy yo\?/i
+      })
+
+      const skills = screen.getByRole('link', {
+        name: /habilidades/i
+      })
+
+      const projects = screen.getByRole('link', {
+        name: /proyectos/i
+      })
+
+      const contact = screen.getByRole('link', {
+        name: /contacto/i
       })
 
       expect(whoIam).toBeInTheDocument();
