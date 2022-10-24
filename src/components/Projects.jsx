d70ae729e4b6b7f310d78e5dc934b3d51841ff51
Elielson Nascimento <nascimento.elielson@gmail.com>
2022-10-24 16:34:26 -0300

    feat - component Products

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import data from '../APIFAKE';
+import tictactoe from '../components/images/tictactoe.gif';
+import lottery from '../components/images/lottery.gif';
+import calculator from '../components/images/calculator.gif';
+import reactredux from '../components/images/reactredux.png';
+import javascriptandes6 from '../components/images/javascriptandes6.png';
 import '../App.css';
 
 function Projects({language}) {
@@ -9,13 +14,59 @@ function Projects({language}) {
             <span className='lineSpan'>___________________________________________</span>
             <h1 className='topicsStyle'>{data[language].project}</h1>
             <article>
-                <div>
-                    <img
-                    src=""
-                    alt="foto do projeto"
-                    />
-                    <h4>Nome do projeto</h4>
-                </div>
+              <div id='myProjects'>
+                <a 
+                  href="https://elielsondev.github.io/tic-tac-toe/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='product'
+                >
+                  <img src={tictactoe} alt="Jogo da Velha" width='200px' />
+                  <p>Tic - Tac - Toe</p>
+                </a>
+
+                <a 
+                  href="https://geradordejogomegasena.vercel.app/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='product'
+                > 
+                  <img src={lottery} alt="Jogo da Velha" width='200px' />
+                  <p>Lottery</p>
+                </a>
+
+                <a 
+                  href="https://elielsondev.github.io/my-calculator/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='product'
+                >
+                  <img src={calculator} alt="Jogo da Velha" width='200px' />
+                  <p>Calculator</p> 
+                </a>
+              </div>
+
+              <div id='myArticles'>
+              <a 
+                  href="https://github.com/elielsondev/redux_baby_steps"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='article'
+                >
+                  <img src={reactredux} alt="Jogo da Velha" width='270px' />
+                  <p>Artigo - Redux in React.js Baby Steps</p>
+                </a>
+
+                <a 
+                  href="https://github.com/elielsondev/starting_javascript"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className='article'
+                >
+                  <img src={javascriptandes6} alt="Jogo da Velha" width='270px' />
+                  <p>Artigo - Starting JavaScript</p>   
+                </a>
+              </div>
             </article>
         </section>
     </div>
