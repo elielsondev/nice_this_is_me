565716ddc6dfa434cb7e6cf506c2390c8b312d64
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-04-14 19:20:12 -0300

    feat - adicionando o VLibras ao portifólio, deixando-o mais acessível para pessoas surdas

--- src/APIFAKE.js
+++ src/APIFAKE.js
@@ -9,7 +9,7 @@ const data = [
         flagUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png',
         flagName: 'Brasil',
         welcome: 'Oi, eu sou',
-        profession: 'Desenvolvedor de Software - Front-End',
+        profession: 'Desenvolvedor de Software - Full Stack',
         description: `Que bom ter você aqui, antes de falar um pouco sobre mim, quero que sinta-se a vontade e pode me
         chamar de Eli: Pois bem, estou em transição de carreira e em 2022 eu conclui o curso de desenvolvimento web full stack
         na Trybe, foram mais de 1500 horas de conteudos teoricos e muita prática, como Fundamentos Web, Front-end, Back-End,
@@ -35,7 +35,7 @@ const data = [
         flagUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png',
         flagName: 'United Kingdom',
         welcome: 'Hi, I am',
-        profession: 'Software developer - Front end',
+        profession: 'Software Developer - Full Stack',
         description: `It's good to have you here, before talking a little about me, I want you to feel free and can
         call Eli: Well, I'm in a career transition and in 2022 I completed the full stack web development course
         at Trybe, there were more than 1500 hours of theoretical content and a lot of practice, such as Web Fundamentals, Front-end, Back-End,
@@ -61,7 +61,7 @@ const data = [
         flagUrl:  'https://upload.wikimedia.org/wikipedia/commons/8/89/Bandera_de_Espa%C3%B1a.svg',
         flagName: 'Spanish',
         welcome: 'Hola soy yo',
-        profession: 'Desenrrollador de software - Interfaz',
+        profession: 'Desarrollador de software - Pila completa',
         description: `Que bueno tenerte por aqui, antes de hablar un poco de mi, quiero que te sientas libre y puedas
         call Eli: Bueno, estoy en una transición de carrera y en 2022 completé el curso de desarrollo web de pila completa
         en Trybe hubo más de 1500 horas de contenido teórico y mucha práctica, como Web Fundamentals, Front-end, Back-End,
