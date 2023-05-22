49a4726d0eae92373bef02760e4cb0581828b316
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-05-22 19:03:56 -0300

    fix - informações pessoais

--- src/APIFAKE.js
+++ src/APIFAKE.js
@@ -14,14 +14,16 @@ const data = [
         chamar de Eli: Pois bem, estou em transição de carreira e em 2022 eu conclui o curso de desenvolvimento web full stack
         na Trybe, foram mais de 1500 horas de conteudos teoricos e muita prática, como Fundamentos Web, Front-end, Back-End,
         Ciência da Computação e as tão importantes Soft-Skills, hoje me sinto pronto para realizar um sonho antigo, que é de
-        trabalhar com Desenvolvimento de softwares.`,
+        trabalhar com Desenvolvimento de softwares, porém não para por ai, sou formado em Gestão de Produção Industrial e 
+        "agora" em 2023 dei início a minha pós-graduação em Engenharia de Software com Métodos Ágeis.`,
         softSkills: [
             "Humildade",
             "Proatividade",
             "Comunicação",
             "Organização",
             "Planejamento",
-            "Trabalho em equipe"
+            "Trabalho em equipe",
+            "Empatia"
         ],
         footer: 'Criado com muito carinho, amor, dedicação, React.js - Por Elielson'
     },
@@ -40,14 +42,17 @@ const data = [
         call Eli: Well, I'm in a career transition and in 2022 I completed the full stack web development course
         at Trybe, there were more than 1500 hours of theoretical content and a lot of practice, such as Web Fundamentals, Front-end, Back-End,
         Computer Science and the so important Soft-Skills, today I feel ready to fulfill an old dream, which is
-        work with software development.`,
+        working with software development, but it doesn't stop there, I have a degree in Industrial Production Management and
+        "now" in 2023 I started my postgraduate degree in Software Engineering with Agile Methods.`,
         softSkills: [
             "Humility",
             "Proactivity",
             "Communication",
             "Organization",
             "Planning",
-            "Teamwork"
+            "Teamwork",
+            "Empathy"
+
         ],
         footer: 'Created with great care, love, dedication, React.js - By Elielson'
     },
@@ -66,14 +71,16 @@ const data = [
         call Eli: Bueno, estoy en una transición de carrera y en 2022 completé el curso de desarrollo web de pila completa
         en Trybe hubo más de 1500 horas de contenido teórico y mucha práctica, como Web Fundamentals, Front-end, Back-End,
         Informática y las tan importantes Soft-Skills, hoy me siento preparado para cumplir un viejo sueño, que es
-        trabajar con el desarrollo de software.`,
+        trabajando con el desarrollo de software, pero no se queda ahí, soy licenciado en Gestión de la Producción Industrial y
+        "ahora" en 2023 comencé mi posgrado en Ingeniería de Software con Métodos Ágiles.`,
         softSkills: [
             "Humildad",
             "Proactividad",
             "Comunicación",
             "Organización",
             "Planificación",
-            "Trabajo en equipo"
+            "Trabajo en equipo",
+            "Empatía"
         ],
         footer: 'Creado con gran cuidado, amor, dedicación, React.js - Por Elielson'
     }
