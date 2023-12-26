1e1bea3d560a8f5c57cf48180e22e67a0c12b7ca
Elielson Nascimento <nascimento.elielson@gmail.com>
2023-12-26 14:44:54 -0300

    refactor - change itens and styles

--- src/APIFAKE.js
+++ src/APIFAKE.js
@@ -9,13 +9,8 @@ const data = [
         flagUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1200px-Flag_of_Brazil.svg.png',
         flagName: 'Brasil',
         welcome: 'Oi, eu sou',
-        profession: 'Engenheiro de Software - Full Stack',
-        description: `Que bom ter você aqui, antes de falar um pouco sobre mim, quero que sinta-se a vontade e pode me
-        chamar de Eli: Pois bem, estou em transição de carreira e em 2022 eu conclui o curso de desenvolvimento web full stack
-        na Trybe, foram mais de 1500 horas de conteudos teoricos e muita prática, como Fundamentos Web, Front-end, Back-End,
-        Ciência da Computação e as tão importantes Soft-Skills, hoje me sinto pronto para realizar um sonho antigo, que é de
-        trabalhar com Desenvolvimento de softwares, porém não para por ai, sou formado em Gestão de Produção Industrial e 
-        "agora" em 2023 dei início a minha pós-graduação em Engenharia de Software com Métodos Ágeis.`,
+        profession: 'Engenheiro de Software Front-end',
+        description: `É um prazer tê-lo por aqui. Antes de compartilhar um pouco sobre mim, quero que se sinta à vontade. estou em transição de carreira desde 2022 ao concluir o curso de desenvolvimento web full stack na Trybe, onde dediquei a mais de 1500 horas de estudos teóricos e práticos, abrangendo temas como Fundamentos Web, Front-end, Back-End, Ciência da Computação e as valiosas Soft-Skills. Atualmente, estou confiante em minha capacidade de realizar um antigo sonho: trabalhar com desenvolvimento de softwares. Além disso, possuo formação em Gestão de Produção Industrial e em Junho de 2023 iniciei minha pós-graduação em Engenharia de Software com Métodos Ágeis, atualmente trabalho como freelancer e busco minha primeira vaga como Engenheiro de Software Frontend`,
         softSkills: [
             "Humildade",
             "Proatividade",
@@ -25,7 +20,7 @@ const data = [
             "Trabalho em equipe",
             "Empatia"
         ],
-        footer: 'Criado com muito carinho, amor, dedicação, React.js - Por Elielson'
+        footer: 'Criado com muito carinho, amor, dedicação, React.js - Por Elielson Nascimento Ramos'
     },
     {
         language: 'English',
@@ -36,14 +31,9 @@ const data = [
         contact: 'Contact',
         flagUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/a/ae/Flag_of_the_United_Kingdom.svg/1200px-Flag_of_the_United_Kingdom.svg.png',
         flagName: 'United Kingdom',
-        welcome: 'Hi, I am',
-        profession: 'Software Engineer - Full Stack',
-        description: `It's good to have you here, before talking a little about me, I want you to feel free and can
-        call Eli: Well, I'm in a career transition and in 2022 I completed the full stack web development course
-        at Trybe, there were more than 1500 hours of theoretical content and a lot of practice, such as Web Fundamentals, Front-end, Back-End,
-        Computer Science and the so important Soft-Skills, today I feel ready to fulfill an old dream, which is
-        working with software development, but it doesn't stop there, I have a degree in Industrial Production Management and
-        "now" in 2023 I started my postgraduate degree in Software Engineering with Agile Methods.`,
+        welcome: `Hi, I'm`,
+        profession: 'Front-end Software Engineer',
+        description: `It's a pleasure to have you here. Before I share a little about myself, I want you to feel at ease. I've been in a career transition since 2022, when I completed the full stack web development course at Trybe, dedicating over 1500 hours to theoretical and practical studies covering topics such as Web Fundamentals, Front-end, Back-End, Computer Science, and valuable Soft-Skills. Currently, I am confident in my ability to pursue an old dream: working in software development. Additionally, I have a background in Industrial Production Management and in June 2023, I began my postgraduate studies in Software Engineering with Agile Methods. I am currently working as a freelancer and seeking my first position as a Frontend Software Engineer.`,
         softSkills: [
             "Humility",
             "Proactivity",
@@ -54,7 +44,7 @@ const data = [
             "Empathy"
 
         ],
-        footer: 'Created with great care, love, dedication, React.js - By Elielson'
+        footer: 'Created with great care, love, dedication, React.js - By Elielson Nascimento Ramos'
     },
     {
         language: 'Espanõl',
@@ -66,13 +56,8 @@ const data = [
         flagUrl:  'https://upload.wikimedia.org/wikipedia/commons/8/89/Bandera_de_Espa%C3%B1a.svg',
         flagName: 'Spanish',
         welcome: 'Hola soy yo',
-        profession: 'Ingeniero de Software - Pila completa',
-        description: `Que bueno tenerte por aqui, antes de hablar un poco de mi, quiero que te sientas libre y puedas
-        call Eli: Bueno, estoy en una transición de carrera y en 2022 completé el curso de desarrollo web de pila completa
-        en Trybe hubo más de 1500 horas de contenido teórico y mucha práctica, como Web Fundamentals, Front-end, Back-End,
-        Informática y las tan importantes Soft-Skills, hoy me siento preparado para cumplir un viejo sueño, que es
-        trabajando con el desarrollo de software, pero no se queda ahí, soy licenciado en Gestión de la Producción Industrial y
-        "ahora" en 2023 comencé mi posgrado en Ingeniería de Software con Métodos Ágiles.`,
+        profession: 'Ingeniero de software front-end',
+        description: `Es un placer tenerte aquí. Antes de compartir un poco sobre mí, quiero que te sientas cómodo. Estoy en transición de carrera desde 2022, cuando completé el curso de desarrollo web full stack en Trybe, dedicando más de 1500 horas a estudios teóricos y prácticos que abarcaron temas como Fundamentos Web, Front-end, Back-End, Ciencia de la Computación y valiosas habilidades blandas. Actualmente, tengo confianza en mi capacidad para perseguir un viejo sueño: trabajar en el desarrollo de software. Además, tengo formación en Gestión de Producción Industrial y en junio de 2023 comencé mis estudios de posgrado en Ingeniería de Software con Métodos Ágiles. Actualmente trabajo como autónomo y estoy buscando mi primer puesto como Ingeniero de Software Frontend.`,
         softSkills: [
             "Humildad",
             "Proactividad",
@@ -82,7 +67,7 @@ const data = [
             "Trabajo en equipo",
             "Empatía"
         ],
-        footer: 'Creado con gran cuidado, amor, dedicación, React.js - Por Elielson'
+        footer: 'Creado con gran cuidado, amor, dedicación, React.js - Por Elielson Nascimento Ramos'
     }
 ];
 
